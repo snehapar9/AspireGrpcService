1380e5d382716d3b4de22b0c88de45d236a0ac71
Dan Vouaux <daniv@microsoft.com>
2023-12-19 18:18:52 -0800

    Add sample stream + watch feature on WatchResources

--- Services/AspireService.cs
+++ Services/AspireService.cs
@@ -1,17 +1,23 @@
 ﻿using Aspire.V1;
 using Grpc.Core;
 using k8s;
+using k8s.Models;
+using Newtonsoft.Json;
 namespace AspireGrpcService.Services
 {
 
     public class AspireService : Aspire.V1.DashboardService.DashboardServiceBase
     {
         private readonly ILogger _logger;
-        private readonly static KubernetesClientConfiguration _config = KubernetesClientConfiguration.InClusterConfig();
-        private readonly static Kubernetes _client = new(_config);
+        private readonly KubernetesClientConfiguration _kubernetesConfig;
+        private readonly Kubernetes _kubernetesClient;
+        private IServerStreamWriter<WatchResourcesUpdate>? _currentWatchResourcesUpdateStream;
+
         public AspireService(ILogger<AspireService> logger) 
         {
             _logger = logger;
+            _kubernetesConfig = KubernetesClientConfiguration.InClusterConfig();
+            _kubernetesClient = new Kubernetes(_kubernetesConfig);
         }
 
         public override Task<ApplicationInformationResponse> GetApplicationInformation(ApplicationInformationRequest request, ServerCallContext context)
@@ -20,9 +26,38 @@ namespace AspireGrpcService.Services
             return base.GetApplicationInformation(request, context);
         }
 
-        public override Task WatchResources(WatchResourcesRequest request, IServerStreamWriter<WatchResourcesUpdate> responseStream, ServerCallContext context)
+        public override async Task WatchResources(WatchResourcesRequest request, IServerStreamWriter<WatchResourcesUpdate> responseStream, ServerCallContext context)
         {
-            return base.WatchResources(request, responseStream, context);
+            _currentWatchResourcesUpdateStream = responseStream;
+
+            // Creates the watcher
+            var podsWatchResponse = _kubernetesClient.CoreV1.ListNamespacedPodWithHttpMessagesAsync("k8se-apps", watch: true);
+            var podWatcher = podsWatchResponse.Watch<V1Pod, V1PodList>(async (type, item) =>
+            {
+                Console.WriteLine($"Pod event of type {type} detected for {item.Metadata.Name}");
+                var reply = new WatchResourcesUpdate()
+                {
+                    InitialData = new InitialResourceData()
+                    {
+                        ResourceTypes = { new ResourceType() { DisplayName = $"{type} pod: {item.Metadata.Name}" } }
+                    }
+                };
+                await _currentWatchResourcesUpdateStream.WriteAsync(reply);
+            });
+
+            // Gets the initial data and return it
+            var podsList = await _kubernetesClient.CoreV1.ListNamespacedPodAsync("k8se-apps");
+            var initialReply = new WatchResourcesUpdate()
+            {
+                InitialData = new InitialResourceData()
+                {
+                    ResourceTypes = { new ResourceType() { DisplayName = $"Initial call. Found {podsList.Items.Count} pods" } }
+                }
+            };
+            await _currentWatchResourcesUpdateStream.WriteAsync(initialReply);
+
+            // Force wait to keep the connection open. TODO: Update
+            await new Task(() => { });
         }
 
         public override async Task WatchResourceConsoleLogs(WatchResourceConsoleLogsRequest request, IServerStreamWriter<WatchResourceConsoleLogsUpdate> responseStream, ServerCallContext context)
@@ -30,7 +65,7 @@ namespace AspireGrpcService.Services
            var podName = request.ResourceId.Uid.ToString();
            while(!context.CancellationToken.IsCancellationRequested)
             {
-                using k8s.Autorest.HttpOperationResponse<Stream> stream = await _client.CoreV1.ReadNamespacedPodLogWithHttpMessagesAsync(podName, _config.Namespace);
+                using k8s.Autorest.HttpOperationResponse<Stream> stream = await _kubernetesClient.CoreV1.ReadNamespacedPodLogWithHttpMessagesAsync(podName, _kubernetesConfig.Namespace);
                 var logsUpdate = new WatchResourceConsoleLogsUpdate();
                 using var reader = new StreamReader(stream.Body);
                 while (!reader.EndOfStream)
