8816b560aa85cc5de2550d7587543bb6d80d2661
Sneha Parthasarathy <snehapar@microsoft.com>
2023-12-22 07:43:21 -0800

    Update proto

--- Services/AspireService.cs
+++ Services/AspireService.cs
@@ -3,6 +3,7 @@ using Grpc.Core;
 using k8s;
 using k8s.Models;
 using Newtonsoft.Json;
+using static Prometheus.Exemplar;
 namespace AspireGrpcService.Services
 {
 
@@ -21,18 +22,34 @@ namespace AspireGrpcService.Services
             _kubernetesClient = new Kubernetes(_kubernetesConfig);
         }
 
-        public override Task<ApplicationInformationResponse> GetApplicationInformation(ApplicationInformationRequest request, ServerCallContext context)
+        public override async Task<ApplicationInformationResponse> GetApplicationInformation(ApplicationInformationRequest request, ServerCallContext context)
         {
             // TODO - Confirm if we can use "tags" to identity Aspire Apps in ACA.
-            return base.GetApplicationInformation(request, context);
+           return await base.GetApplicationInformation(request, context).ConfigureAwait(false);
+            
         }
 
         public override async Task WatchResources(WatchResourcesRequest request, IServerStreamWriter<WatchResourcesUpdate> responseStream, ServerCallContext context)
         {
             _currentWatchResourcesUpdateStream = responseStream;
 
+            // Creates the watcher
+            var podsWatchResponse = _kubernetesClient.CoreV1.ListNamespacedPodWithHttpMessagesAsync("k8se-apps", watch: true);
+            var podWatcher = podsWatchResponse.Watch<V1Pod, V1PodList>(async (type, item) =>
+            {
+                Console.WriteLine($"Pod event of type {type} detected for {item.Metadata.Name}");
+                var reply = new WatchResourcesUpdate()
+                {
+                    InitialData = new InitialResourceData()
+                    {
+                        ResourceTypes = { new ResourceType() { DisplayName = $"{type} pod: {item.Metadata.Name}" } }
+                    }
+                };
+                await responseStream.WriteAsync(reply);
+            });
+
             // Gets the initial data and return it
-            var podsList = await _kubernetesClient.CoreV1.ListNamespacedPodAsync(_kubernetesConfig.Namespace);
+            var podsList = await _kubernetesClient.CoreV1.ListNamespacedPodAsync("k8se-apps");
             var initialReply = new WatchResourcesUpdate()
             {
                 InitialData = new InitialResourceData()
@@ -40,34 +57,11 @@ namespace AspireGrpcService.Services
                     ResourceTypes = { new ResourceType() { DisplayName = $"Initial call. Found {podsList.Items.Count} pods" } }
                 }
             };
-            if (string.IsNullOrEmpty(_resourceVersion))
-            {
-                _resourceVersion = podsList.Metadata.ResourceVersion;
-                await responseStream.WriteAsync(initialReply);
-            }
-            try
-            {
-                var podsWatchResponse = _kubernetesClient.CoreV1.ListNamespacedPodWithHttpMessagesAsync(_kubernetesConfig.Namespace, resourceVersion:_resourceVersion, resourceVersionMatch: _resourceVersion,  watch: true);
-                var podWatcher = podsWatchResponse.Watch<V1Pod, V1PodList>(async (type, item) =>
-                {
-                    Console.WriteLine($"Pod event of type {type} detected for {item.Metadata.Name}");
-                    var reply = new WatchResourcesUpdate()
-                    {
-                       // Populate changes from events and write to writer
-                       Changes = { }
-                    };
-                    await responseStream.WriteAsync(reply);
-                });
-            }
-            catch (Exception ex)
-            {
-                _resourceVersion = null;
-                Console.WriteLine(ex.ToString());
-                // TODO : Cancel request 
-            }
-            
+
+            await responseStream.WriteAsync(initialReply);
+
             // Wait until the cancellation is requested
-            while (!context.CancellationToken.IsCancellationRequested || !request.IsReconnect)
+            while (!context.CancellationToken.IsCancellationRequested)
             {
                 await Task.Delay(TimeSpan.FromMinutes(1), context.CancellationToken).ContinueWith(task => { });
             }
@@ -76,12 +70,14 @@ namespace AspireGrpcService.Services
 
         public override async Task WatchResourceConsoleLogs(WatchResourceConsoleLogsRequest request, IServerStreamWriter<WatchResourceConsoleLogsUpdate> responseStream, ServerCallContext context)
         {
-           var podName = request.ResourceId.Uid.ToString();
+           var label = $"app={request.ResourceName}";
+           var pods = await _kubernetesClient.CoreV1.ListNamespacedPodAsync(_kubernetesConfig.Namespace, labelSelector: label);
+           var podName = pods.Items[0].Metadata.Name;
            while(!context.CancellationToken.IsCancellationRequested)
             {
-                using k8s.Autorest.HttpOperationResponse<Stream> stream = await _kubernetesClient.CoreV1.ReadNamespacedPodLogWithHttpMessagesAsync(podName, _kubernetesConfig.Namespace);
+                var stream = await _kubernetesClient.CoreV1.ReadNamespacedPodLogAsync(podName, _kubernetesConfig.Namespace);
                 var logsUpdate = new WatchResourceConsoleLogsUpdate();
-                using var reader = new StreamReader(stream.Body);
+                using var reader = new StreamReader(stream);
                 while (!reader.EndOfStream)
                 {
                     var logEntry = await reader.ReadLineAsync();
